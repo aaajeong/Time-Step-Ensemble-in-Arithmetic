83051e6b38ec779906a54f6130f1b27cecd9c354
Ahjeong Park <qkrdkwjd59@naver.com>
2021-09-15 17:54:31 +0900

    Update training model code (additiong) - using gpu - save accuracy img

--- test/ch07/train_seq2seq.py
+++ test/ch07/train_seq2seq.py
@@ -4,14 +4,18 @@ sys.path.append('./')
 import numpy as np
 import matplotlib.pyplot as plt
 from dataset import sequence
+from common import config
 from common.optimizer import Adam
 from common.trainer import Trainer
-from common.util import eval_seq2seq
+from common.util import eval_seq2seq, to_cpu, to_gpu
 from seq2seq import Seq2seq
 from peeky_seq2seq import PeekySeq2seq
 from tensorflow.python.client import device_lib
 print(device_lib.list_local_devices())
 
+# GPU에서 실행하려면 아래 주석을 해제하세요(CuPy 필요).
+# ===============================================
+# config.GPU = True
 
 # 데이터셋 읽기
 (x_train, t_train), (x_test, t_test) = sequence.load_data('addition.txt')
@@ -40,6 +44,12 @@ optimizer = Adam()
 trainer = Trainer(model, optimizer)
 
 acc_list = []
+
+# gpu로 데이터 읽기
+if config.GPU:
+    x_train, t_train = to_gpu(x_train), to_gpu(t_train)
+
+# max_epoch = 2
 for epoch in range(max_epoch):
     trainer.fit(x_train, t_train, max_epoch=1,
                 batch_size=batch_size, max_grad=max_grad)
@@ -55,12 +65,15 @@ for epoch in range(max_epoch):
     acc_list.append(acc)
     print('검증 정확도 %.3f%%' % (acc * 100))
 
-model.save_params('model.pkl')
+model.save_params('addition_sc.pkl')
+
 # 그래프 그리기
 x = np.arange(len(acc_list))
 plt.plot(x, acc_list, marker='o')
-plt.xlabel('에폭')
-plt.ylabel('정확도')
+plt.title('Addition Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
 plt.ylim(0, 1.0)
+plt.savefig('addition_Accuracy.png')
 plt.show()
 
